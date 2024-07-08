5a0258ac85022632f91d65341c3ec670e8a0e280
ryanlyang <ryan_l_yang@brown.edu>
2024-07-08 01:16:00 -0500

    feeling better now

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,23 +11,41 @@ const Header: React.FC = () => {
         <ul>
         <li>
             <div className="button-container">
-            <Link to={`/`}>Fixed Header</Link>
+              <div className="hover-image-container-top hover-image-container-home">
+                <img src="/Gold glow resize.png" alt="Hover Image" className=
+                "hover-image-top home-site-click"></img>
+              </div>
+              <Link to={`/`}>Fixed Header</Link>
+              <div className="hover-image-container-bottom hover-image-container-home">
+                <img src="/Gold glow resize.png" alt="Hover Image" className=
+                "hover-image-bottom home-site-click"></img>
+              </div>
             </div>
           </li>
           <li>
             <div className="button-container">
-              <div className="hover-image-container">
-                <img src="/Gold glow resize.png" alt="Hover Image" className="hover-image-top"></img>
+              <div className="hover-image-container-top hover-image-container-about">
+                <img src="/Gold glow resize.png" alt="Hover Image" className=
+                "hover-image-top about-click"></img>
               </div>
               <Link to={`about`}>About</Link>
-              <div className="hover-image-container">
-                <img src="/Gold glow resize.png" alt="Hover Image" className="hover-image-bottom"></img>
+              <div className="hover-image-container-bottom hover-image-container-about">
+                <img src="/Gold glow resize.png" alt="Hover Image" className=
+                "hover-image-bottom about-click"></img>
               </div>
             </div>
           </li>
           <li>
             <div className='button-container'>
+              <div className="hover-image-container-top hover-image-container-gallery">
+                <img src="/Gold glow resize.png" alt="Hover Image" className=
+                "hover-image-top gallery-click"></img>
+              </div>
               <Link to={'gallery'}>Gallery</Link>
+              <div className="hover-image-container-bottom hover-image-container-gallery">
+                <img src="/Gold glow resize.png" alt="Hover Image" className=
+                "hover-image-bottom gallery-click"></img>
+              </div>
             </div>
           </li>
         </ul>
