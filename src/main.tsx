59639371e340341bd7a92ccdd26c72b27017f76c
ryanlyang <ryan_l_yang@brown.edu>
2024-07-08 01:53:06 -0500

    practice

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import ErrorPage from "./pages/error-page.tsx";
 import About from "./pages/about.tsx";
 import Home from "./pages/home.tsx";
 import Gallery from "./pages/gallery.tsx";
-// import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 Amplify.configure(outputs);
 
@@ -38,27 +38,27 @@ const router = createBrowserRouter([
   }
 ]);
 
-// const theme = extendTheme({
-//   styles: {
-//     global: {
-//       body: {
-//         bg: "black",
-//         color: "white",
-//         minHeight: "100vh",
-//         margin: "0",
-//         padding: "0",
-//       },
-//       html: {
-//         height: "100%",
-//       },
-//     },
-//   },
-// });
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "black",
+        color: "white",
+        minHeight: "100vh",
+        margin: "0",
+        padding: "0",
+      },
+      html: {
+        height: "100%",
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <ChakraProvider theme={theme}> */}
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
-    {/* </ChakraProvider> */}
+    </ChakraProvider>
   </React.StrictMode>
 );
