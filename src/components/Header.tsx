c248b8c689da79f31dfd10eb2ee5b8624beb928e
ryanlyang <ryan_l_yang@brown.edu>
2024-07-09 01:43:22 -0500

    going

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,28 +5,32 @@ import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   // Function to handle mouse over
-  const handleMouseOver = () => {
-    const facebookIcon = document.getElementById('facebook-icon') as HTMLImageElement;
+  // Function to handle mouse over
+  const handleMouseOver = (id :string, picture :string) => {
+    const facebookIcon = document.getElementById(id) as HTMLImageElement;
     if (facebookIcon) {
       facebookIcon.classList.add('hovered'); // Add class for transition
       setTimeout(() => {
-        facebookIcon.src = '/real gold facebook.png'; // Path to hover image
+        facebookIcon.src = picture; // Path to hover image
         facebookIcon.classList.remove('hovered'); // Remove class after transition
       }, 125); // Half of the transition duration for smooth effect
     }
   };
 
   // Function to handle mouse out
-  const handleMouseOut = () => {
-    const facebookIcon = document.getElementById('facebook-icon') as HTMLImageElement;
+  const handleMouseOut = (id :string, picture :string) => {
+    const facebookIcon = document.getElementById(id) as HTMLImageElement;
     if (facebookIcon) {
       facebookIcon.classList.add('hovered'); // Add class for transition
       setTimeout(() => {
-        facebookIcon.src = '/black facebook.png'; // Path to original image
+        facebookIcon.src = picture; // Path to original image
         facebookIcon.classList.remove('hovered'); // Remove class after transition
       }, 125); // Half of the transition duration for smooth effect
     }
   };
+
+
+  
 
   return (
     <header className="fixed-header">
@@ -105,15 +109,26 @@ const Header: React.FC = () => {
                 "hover-image-top gallery-click"></img>
               </div> */}
               <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" 
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut} className='button-container-a'>
-                <img id="facebook-icon" src="/black facebook.png" alt="Facebook Logo" className="facebook"></img>
+                onMouseOver={() => handleMouseOver('facebook-icon', '/real gold facebook.png')} 
+                onMouseOut={() => handleMouseOut('facebook-icon', '/black facebook.png')} className='button-container-a'>
+                <img id="facebook-icon" src="/black facebook.png" alt="Facebook Logo" className="facebook"
+                ></img>
+                <div className='overlay'></div>
               </a>
               {/* <div className="hover-image-container-bottom hover-image-container-gallery">
                 <img src="/Gold glow resize.png" alt="Hover Image" className=
                 "hover-image-bottom gallery-click"></img>
               </div> */}
             {/* </div> */}
+          </li>
+          <li>
+              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" 
+                onMouseOver={() => handleMouseOver('instagram-icon', '/Instagram_logo_gold.png')} 
+                onMouseOut={() => handleMouseOut('instagram-icon', '/Instagram_logo_gray.png')} className='button-container-a'>
+                <img id="instagram-icon" src="/Instagram_logo_gray.png" alt="Instagram Logo" className="facebook"
+                ></img>
+                <div className='overlay'></div>
+              </a>
           </li>
         </ul>
       </nav>
