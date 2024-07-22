7baa033aab8a07b4524f8726e39826e276142bf0
ryanlyang <ryan_l_yang@brown.edu>
2024-07-21 19:30:51 -0500

    and I didn't save it

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { Box, Text, HStack, VStack, IconButton, Heading } from '@chakra-ui/react';
 import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
